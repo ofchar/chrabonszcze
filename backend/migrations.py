ef4d22e51ac2034833da49105273dcb2ee4fd385
ofchar <matowczarek@outlook.com>
2022-11-08 22:35:03 +0000

    select users query

--- backend/migrations.py
+++ backend/migrations.py
@@ -1,21 +1,19 @@
 CREATE_USERS_TABLE = (
     """CREATE TABLE IF NOT EXISTS users (
-        id INT GENERATED ALWAYS AS IDENTITY,
+        id BIGSERIAL PRIMARY KEY,
         name TEXT NOT NULL,
         email TEXT NOT NULL,
         password TEXT NOT NULL,
-        token TEXT,
-        PRIMARY KEY(id)
+        token TEXT
     );"""
 )
 
 CREATE_HAPPINESS_RECORDINGS_TABLE = (
     """CREATE TABLE IF NOT EXISTS happiness_recordings (
-        id INT GENERATED ALWAYS AS IDENTITY,
+        id BIGSERIAL PRIMARY KEY,
         user_id INT NOT NULL,
         record_date DATE NOT NULL,
         happiness_level REAL DEFAULT 0 NOT NULL,
-        PRIMARY KEY(id),
         CONSTRAINT fk_user
             FOREIGN KEY(user_id)
                 REFERENCES users(id)
---
+++ backend/queries.py
@@ -0,0 +1,3 @@
+SELECT_FROM_USERS_BY_ID = (
+    """SELECT * FROM users WHERE id = (%s);"""
+)
