93404976c31699d6f9050b17464a3bb4fb95f17a
ofchar <matowczarek@outlook.com>
2022-11-08 22:19:25 +0000

    i made something (database)

--- backend/init.py
+++ backend/init.py
@@ -1,14 +1,38 @@
 from flask import Flask, render_template
 import os
+from dotenv import load_dotenv
+import psycopg2
 
+from migrations import runMigrations
+
+
+
+# SETUP
+load_dotenv()
 app = Flask(__name__)
 
+connection = psycopg2.connect(
+    host=os.environ.get("DATABASE_URL"),
+    dbname=os.environ.get("DATABASE_NAME"),
+    user=os.environ.get("DATABASE_USERNAME"),
+    password=os.environ.get("DATABASE_PASSWORD")
+)
 
+
+
+
+# ROUTING
 @app.route('/')
 def home():
-    return "TEST"
+    return "TESTXD"
 
 
+
+
+# I HATE PYTHON
 if __name__ == "__main__":
+    runMigrations(connection)
+
     port = int(os.environ.get('PORT', 5000))
+
     app.run(debug=True, host='0.0.0.0', port=port)
