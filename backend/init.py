984cc2143951ea16eba04d5b47dcc843316a654e
ofchar <matowczarek@outlook.com>
2022-11-08 22:55:18 +0000

    register working

--- backend/init.py
+++ backend/init.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
 import os
 from dotenv import load_dotenv
 import psycopg2
 
 from migrations import runMigrations
-from queries import SELECT_FROM_USERS_BY_ID
+from queries import SELECT_FROM_USERS_BY_ID, ADD_USER
+from utils import getRandomString
 
 
 
 # SETUP
 load_dotenv()
+
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/' # secret! dont look at it!
 
 connection = psycopg2.connect(
     host=os.environ.get("DATABASE_URL"),
@@ -28,13 +31,27 @@ def home():
     return "TESTXD"
 
 @app.route('/users/<int:id>')
-def user(id):
+def show_user(id):
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(SELECT_FROM_USERS_BY_ID, (id,))
             record = cursor.fetchone()
             print(record)
     return {"id": record[0], "name": record[1], "email": record[2], "token": record[3]}, 200
+
+@app.route('/register', methods=['POST'])
+def store_user():
+    token = getRandomString(128)
+    print(token)
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(ADD_USER, (
+                request.json['name'],
+                request.json['email'],
+                request.json['password'], # TODO Hash it xd
+                token
+            ))
+    return 'ok', 201
 
 
 
--- backend/queries.py
+++ backend/queries.py
@@ -1,3 +1,7 @@
 SELECT_FROM_USERS_BY_ID = (
     """SELECT * FROM users WHERE id = (%s);"""
 )
+
+ADD_USER = (
+    """INSERT INTO users (name, email, password, token) VALUES (%s, %s, %s, %s)"""
+)
