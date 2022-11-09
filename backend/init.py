568142ce756cfd2ea9a15d6e1a496b7bafbb306c
ofchar <matowczarek@outlook.com>
2022-11-09 20:51:42 +0000

    HATE

--- backend/init.py
+++ backend/init.py
@@ -15,8 +15,6 @@ from utils import getRandomString
 # SETUP
 load_dotenv()
 
-app = Flask(__name__)
-
 salt = bcrypt.gensalt() # but i prefer pepper
 
 connection = psycopg2.connect(
@@ -28,153 +26,158 @@ connection = psycopg2.connect(
 
 
 
-
-# ROUTING
-@app.route('/')
-def home():
-    return "TESTXD"
-
-@app.route('/register', methods=['POST'])
-def register():
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(queries.SELECT_FROM_USERS_BY_EMAIL, (
-                request.json['email'],
-            ))
-            users = cursor.fetchall()
-    if len(users) > 0:
-        return 'user with such email already exists', 409
-
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(queries.ADD_USER, (
-                request.json['name'],
-                request.json['email'],
-                str(bcrypt.hashpw(request.json['password'].encode('utf-8'), salt).decode('utf8')),
-                getRandomString(169)
-            ))
-    return 'its good byczq', 201
-
-@app.route('/login', methods=['POST'])
-def login():
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(queries.LOGIN, (
-                request.json['email'],
-            ))
-            user = cursor.fetchone()
-    if user:
-        if bcrypt.checkpw(request.json['password'].encode('utf-8'), user[3].encode('utf-8')):
-            return {"name": user[1], "email": user[2], "token": user[4]}, 200
-        else:
-            return 'wrong password byczq', 403
-    else:
-        return 'wrong email byczq', 404
-
-# logout is just reseting the user's token. you know. sEcURiTy.
-@app.route('/logout', methods=['POST'])
-def logout():
-    if not 'token' in request.json:
-        return 'no token byczq', 400
-
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(queries.LOGOUT, (
-                getRandomString(169),
-                request.json['token']
-            ))
-    return 'its good byczq', 200
-
-
-@app.route('/api/record', methods=['POST'])
-def recordMessage():
-    if not 'token' in request.json:
-        return 'no token byczq', 400
-
-    if not 'message' in request.json:
-        return 'no message byczq', 400
-
-    message = request.json['message']
-
-    # Dear @mlExperts, in line below we need to assign a value -1, 0 or 1 to the happinessValue based on message variable
-    # (which obviously is message from the user). -1 for negative, 0 for neutral and 1 for positive.
-    # After that is working please remove the randint line.
-    # happinessValue = MLMODEL(message)
-    happinessValue = randint(-1, 1)
-
-    happinessValue = happinessValue / 50
-
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(queries.GET_TODAYS_HAPPINESS_RECORD_FOR_USER, (
-                request.json['token'],
-            ))
-            recording = cursor.fetchone()
-
-    if recording:
-        # recording for today found, we can just update it
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute(queries.UPDATE_TODAYS_HAPPINESS_RECORD_FOR_USER, (
-                    0 + happinessValue,
-                    request.json['token']
-                ))
-    else:
-        # recording for today not found, we need to create it
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute(queries.CREATE_TODAYS_HAPPINESS_RECORD_FOR_USER, (
-                    happinessValue,
-                    request.json['token']
-                ))
-
-    return 'its good byczq', 201
-
-@app.route('/api/happiness-today', methods=['GET'])
-def getHappinessToday():
-    if not 'token' in request.json:
-        return 'no token byczq', 400
-
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(queries.GET_TODAYS_HAPPINESS_RECORD_FOR_USER, (
-                request.json['token'],
-            ))
-            recording = cursor.fetchone()
-    if recording:
-        return {"date": recording[2], "value": recording[3]}
-    else:
-        return {"date": NULL, "value": 0}
-
-@app.route('/api/happiness', methods=['GET'])
-def getHappiness():
-    if not 'token' in request.json:
-        return 'no token byczq', 400
-
-    data = []
-
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(queries.GET_TWO_WEEK_HAPPINESS_RECORDS_FOR_USER, (
-                request.json['token'],
-            ))
-            recordings = cursor.fetchall()
-
-    for record in recordings:
-        result = {
-            "date": record[2],
-            "value": record[3]
-        }
-        data.append(result)
-
-    return {"data": data}
-
-
-
 # I HATE PYTHON
 if __name__ == "__main__":
     runMigrations(connection)
 
     port = int(os.environ.get('PORT', 5000))
 
+    app = create_app
+
     app.run(debug=True, host='0.0.0.0', port=port)
+
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return "TESTXD"
+
+    @app.route('/register', methods=['POST'])
+    def register():
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(queries.SELECT_FROM_USERS_BY_EMAIL, (
+                    request.json['email'],
+                ))
+                users = cursor.fetchall()
+        if len(users) > 0:
+            return 'user with such email already exists', 409
+
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(queries.ADD_USER, (
+                    request.json['name'],
+                    request.json['email'],
+                    str(bcrypt.hashpw(request.json['password'].encode('utf-8'), salt).decode('utf8')),
+                    getRandomString(169)
+                ))
+        return 'its good byczq', 201
+
+    @app.route('/login', methods=['POST'])
+    def login():
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(queries.LOGIN, (
+                    request.json['email'],
+                ))
+                user = cursor.fetchone()
+        if user:
+            if bcrypt.checkpw(request.json['password'].encode('utf-8'), user[3].encode('utf-8')):
+                return {"name": user[1], "email": user[2], "token": user[4]}, 200
+            else:
+                return 'wrong password byczq', 403
+        else:
+            return 'wrong email byczq', 404
+
+    # logout is just reseting the user's token. you know. sEcURiTy.
+    @app.route('/logout', methods=['POST'])
+    def logout():
+        if not 'token' in request.json:
+            return 'no token byczq', 400
+
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(queries.LOGOUT, (
+                    getRandomString(169),
+                    request.json['token']
+                ))
+        return 'its good byczq', 200
+
+
+    @app.route('/api/record', methods=['POST'])
+    def recordMessage():
+        if not 'token' in request.json:
+            return 'no token byczq', 400
+
+        if not 'message' in request.json:
+            return 'no message byczq', 400
+
+        message = request.json['message']
+
+        # Dear @mlExperts, in line below we need to assign a value -1, 0 or 1 to the happinessValue based on message variable
+        # (which obviously is message from the user). -1 for negative, 0 for neutral and 1 for positive.
+        # After that is working please remove the randint line.
+        # happinessValue = MLMODEL(message)
+        happinessValue = randint(-1, 1)
+
+        happinessValue = happinessValue / 50
+
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(queries.GET_TODAYS_HAPPINESS_RECORD_FOR_USER, (
+                    request.json['token'],
+                ))
+                recording = cursor.fetchone()
+
+        if recording:
+            # recording for today found, we can just update it
+            with connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(queries.UPDATE_TODAYS_HAPPINESS_RECORD_FOR_USER, (
+                        0 + happinessValue,
+                        request.json['token']
+                    ))
+        else:
+            # recording for today not found, we need to create it
+            with connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(queries.CREATE_TODAYS_HAPPINESS_RECORD_FOR_USER, (
+                        happinessValue,
+                        request.json['token']
+                    ))
+
+        return 'its good byczq', 201
+
+    @app.route('/api/happiness-today', methods=['GET'])
+    def getHappinessToday():
+        if not 'token' in request.json:
+            return 'no token byczq', 400
+
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(queries.GET_TODAYS_HAPPINESS_RECORD_FOR_USER, (
+                    request.json['token'],
+                ))
+                recording = cursor.fetchone()
+        if recording:
+            return {"date": recording[2], "value": recording[3]}, 200
+        else:
+            return {"date": NULL, "value": 0}, 200
+
+    @app.route('/api/happiness', methods=['GET'])
+    def getHappiness():
+        if not 'token' in request.json:
+            return 'no token byczq', 400
+
+        data = []
+
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(queries.GET_TWO_WEEK_HAPPINESS_RECORDS_FOR_USER, (
+                    request.json['token'],
+                ))
+                recordings = cursor.fetchall()
+
+        for record in recordings:
+            result = {
+                "date": record[2],
+                "value": record[3]
+            }
+            data.append(result)
+
+        return {"data": data}, 200
+
+    return app
