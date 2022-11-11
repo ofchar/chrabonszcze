971b4c767adc1a9c8b236652e6f3b72b38363c73
Emphyr00 <emphyr00@gmail.com>
2022-11-11 15:02:02 +0000

    you

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,19 @@
 <template>
-    <router-view></router-view>
+    <navbar-component></navbar-component>
+    <div class="container w-100 h-100" style="padding-top: 100px">
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
+import NavbarComponent from './components/NavbarComponent.vue';
 export default {
-
+    components: {
+        NavbarComponent,
+    }
 }
 </script>
 
 <style>
-
+@import './css/main.css'
 </style>
