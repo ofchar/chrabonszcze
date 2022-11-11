261991b7f2bea96932d86f7580751034b96225d3
Emphyr00 <emphyr00@gmail.com>
2022-11-11 15:02:16 +0000

    give

---
+++ frontend/src/components/NavbarComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="navbar-container">
+        <div class="custom-navbar-left">
+            <div class="d-flex" style="gap: 20px">
+                <button class="button-custom">Home</button>
+            </div>
+        </div>
+        <div class="custom-navbar-right">
+            <div class="d-flex" style="gap: 20px;">
+                <button class="button-custom">Login</button>
+                <button class="button-custom">Register</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+@import '../css/navbar.css';
+</style>
