40cbb510c2c8b8c2c5a14a240e7a4bfeffa8f96d
emphyr00 <emphyr00@gmail.com>
2022-11-16 23:02:05 +0000

    SASSY loader

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from '../src/router';
-
+import Axios from 'axios'
 
 const router = createRouter({
     // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
@@ -13,6 +13,8 @@ const router = createRouter({
 import 'bootstrap/dist/css/bootstrap.css';
 
 const app = createApp(App)
+
+app.config.globalProperties.axios = Axios;
 
 app.use(router);
 
